c10aa5c8ddab88d7e1cd855cfd2c208ef9643069
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-25 20:38:29 -0300

    Refactor CategoriaRepository imports and formatting

--- src/main/java/org/serratec/coldmart/repository/CategoriaRepository.java
+++ src/main/java/org/serratec/coldmart/repository/CategoriaRepository.java
@@ -1,12 +1,12 @@
 package org.serratec.coldmart.repository;
 
-import com.residence.ecommerce.entity.Categorias;
+import org.serratec.coldmart.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria,UUID>{
- List<Categoria>findByNomeIgnoreCase(String nome);
+public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
+   List<Categoria> findByNomeIgnoreCase(String nome);
 }
