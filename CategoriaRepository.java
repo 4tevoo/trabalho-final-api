3bbec5ec33fea45c9c2bae914aa43e1740a361bd
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-25 21:20:30 -0300

    Update CategoriaRepository.java

--- CategoriaRepository.java
+++ CategoriaRepository.java
@@ -1,15 +1,13 @@
 package org.serratec.coldmart.repository;
 
-import com.residence.ecommerce.entity.Categorias;
+import org.serratec.coldmart.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
-public interface CategoriaRepository extends JpaRepository<Categorias,UUID>{
-    List<Categorias> findByFrontEnd(String FrontEnd);
-    List<Categorias> findByBackEnd(String BackEnd);
-    List<Categorias> findByMobile(String Mobile);
-    List<Categorias> findByFundametos(String Fundamentos);
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
 
+    List<Categoria> findByNomeIgnoreCase(String nome);
 }
