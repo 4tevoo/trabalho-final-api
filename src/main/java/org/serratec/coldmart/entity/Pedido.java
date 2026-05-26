ba3c04f689d57fdd47e18b2dee6d1ce0b07e1ab4
Tevo <estevao.cunha@residente.serratec.org>
2026-05-26 02:09:20 -0300

    Mudando o tipo da dataPedido por compatibilidade

--- src/main/java/org/serratec/coldmart/entity/Pedido.java
+++ src/main/java/org/serratec/coldmart/entity/Pedido.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -22,7 +22,7 @@ public class Pedido {
     private UUID id;
 
     @Column(nullable = false)
-    private Instant dataPedido;
+    private LocalDate dataPedido;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
