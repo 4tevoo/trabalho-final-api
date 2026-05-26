a6fda12aae058c0bc495f17d31f029d977a59c7a
Y-M-dev <yuri.martins045@gmail.com>
2026-05-26 16:32:48 -0300

    Feito RegraNegocioException

---
+++ src/main/java/org/serratec/coldmart/exceptions/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package org.serratec.coldmart.exceptions;
+
+public class RegraNegocioException extends RuntimeException {
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
