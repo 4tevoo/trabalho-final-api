531d72f1ca649042b776788a4d2f813e857405ed
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-27 15:59:46 -0300

    Remove unused method for finding pedidos by status

--- src/main/java/org/serratec/coldmart/repository/PedidoRepository.java
+++ src/main/java/org/serratec/coldmart/repository/PedidoRepository.java
@@ -1,8 +1,6 @@
 package org.serratec.coldmart.repository;
 
 import org.serratec.coldmart.entity.Pedido;
-import org.serratec.coldmart.enums.StatusPagamento;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -10,7 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
-    List<Pedido> findByStatusOrderByDataPedidoDesc(StatusPagamento status, Pageable pageable);
-
     List<Pedido> findByClienteId(UUID clienteId);
 }
