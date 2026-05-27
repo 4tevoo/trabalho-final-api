7b8c30afa4044c14032859ae9c231e45271e4626
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-27 15:57:43 -0300

    Remove unused List import from ClienteRepository
    Removed unused import for List in ClienteRepository.

--- src/main/java/org/serratec/coldmart/repository/ClienteRepository.java
+++ src/main/java/org/serratec/coldmart/repository/ClienteRepository.java
@@ -3,7 +3,6 @@ package org.serratec.coldmart.repository;
 import org.serratec.coldmart.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
