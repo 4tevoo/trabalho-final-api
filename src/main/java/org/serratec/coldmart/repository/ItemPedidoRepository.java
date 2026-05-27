c1d73ababc356b9a274c5b7fbed38f22ad4f37a7
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-27 15:58:58 -0300

    Clean up ItemPedidoRepository by removing unused code
    Removed unused imports and methods from ItemPedidoRepository.

--- src/main/java/org/serratec/coldmart/repository/ItemPedidoRepository.java
+++ src/main/java/org/serratec/coldmart/repository/ItemPedidoRepository.java
@@ -1,16 +1,11 @@
 package org.serratec.coldmart.repository;
 
 import org.serratec.coldmart.entity.ItemPedido;
-import org.serratec.coldmart.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 import java.util.UUID;
 
 @Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, UUID> {
-    List<ItemPedido> findByPedido(Pedido pedido);
-
-    long countByPedidoId(UUID pedidoId);
-    List<ItemPedido> findByDescontoNotNull();
 }
