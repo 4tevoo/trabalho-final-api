2060a6e54bd42b8812292c8f6c11ffc0a8242b2f
LeiltonBraga <leilton.ab@gmail.com>
2026-05-26 21:37:33 -0300

    Implementando o swagger dos controllers e alterando example do model

--- src/main/java/org/serratec/coldmart/controller/ClienteController.java
+++ src/main/java/org/serratec/coldmart/controller/ClienteController.java
@@ -1,10 +1,15 @@
 package org.serratec.coldmart.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.serratec.coldmart.model.CategoriaBuscar;
-import org.serratec.coldmart.model.CategoriaCriar;
-import org.serratec.coldmart.model.ClienteBuscar;
-import org.serratec.coldmart.model.ClienteCriar;
+import org.serratec.coldmart.model.*;
 import org.serratec.coldmart.service.ClienteService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Cliente")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
@@ -24,30 +30,68 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    @Operation(summary = "Cadastrar cliente")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Cliente cadastrado com sucesso", responseCode = "201"),
+            @ApiResponse(description = "Dados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados já cadastrados", responseCode = "409", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @PostMapping
     public ResponseEntity<Void> cadastrarCliente(@RequestBody @Valid ClienteCriar dto) {
         clienteService.cadastrarCliente(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Listar todos os clientes")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Todos os clientes listados com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClienteBuscar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @GetMapping
     public ResponseEntity<List<ClienteBuscar>> listarTodosClientes() {
         List<ClienteBuscar> clientes = clienteService.listarTodos();
         return ResponseEntity.ok(clientes);
     }
 
+    @Operation(summary = "Buscar cliente por ID")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Buscar pelo id do cliente")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Cliente retornado com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClienteBuscar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @GetMapping("/{id}")
     public ResponseEntity<ClienteBuscar> buscarClientePorId(@PathVariable UUID id) {
         ClienteBuscar cliente = clienteService.buscarPorId(id);
         return ResponseEntity.ok(cliente);
     }
 
+    @Operation(summary = "Atualizar cliente")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Atualizar pelo id do cliente")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Cliente atualizado com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ClienteCriar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados já cadastrados", responseCode = "409", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @PutMapping("/{id}")
     public ResponseEntity<ClienteBuscar> editarCliente(@PathVariable UUID id, @RequestBody @Valid ClienteCriar dto) {
         ClienteBuscar clienteEditado = clienteService.editarCliente(id, dto);
         return ResponseEntity.ok(clienteEditado);
     }
 
+    @Operation(summary = "Remover cliente")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Remover pelo id do cliente")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Cliente removido com sucesso", responseCode = "204"),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarCliente(@PathVariable UUID id) {
         clienteService.deletarCliente(id);
--- src/main/java/org/serratec/coldmart/controller/CursoController.java
+++ src/main/java/org/serratec/coldmart/controller/CursoController.java
@@ -1,10 +1,16 @@
 package org.serratec.coldmart.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.serratec.coldmart.entity.Curso;
-import org.serratec.coldmart.model.ClienteBuscar;
-import org.serratec.coldmart.model.CursoBuscar;
-import org.serratec.coldmart.model.CursoCriar;
+import org.serratec.coldmart.model.*;
 import org.serratec.coldmart.service.CursoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Curso")
 @RestController
 @RequestMapping("/api/cursos")
 public class CursoController {
@@ -23,30 +30,67 @@ public class CursoController {
         this.cursoService = cursoService;
     }
 
+    @Operation(summary = "Cadastrar curso")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Curso cadastrado com sucesso", responseCode = "201"),
+            @ApiResponse(description = "Dados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados já cadastrados", responseCode = "409", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @PostMapping
     public ResponseEntity<Void> cadastrarCurso(@RequestBody @Valid CursoCriar dto) {
         cursoService.cadastrarCurso(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Listar todos os cursos")
+    @ApiResponses(value = {
+            @ApiResponse(description = "Todos os curso listados com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CursoBuscar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @GetMapping
     public ResponseEntity<List<CursoBuscar>> listarTodosCursos() {
         List<CursoBuscar> cursos = cursoService.listarTodos();
         return ResponseEntity.ok(cursos);
     }
 
+    @Operation(summary = "Buscar curso pelo id do curso")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Buscar por id")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Curso retornado com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CursoBuscar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @GetMapping("/{id}")
     public ResponseEntity<CursoBuscar> buscarCursoPorId(@PathVariable UUID id) {
         CursoBuscar curso = cursoService.buscarPorId(id);
         return ResponseEntity.ok(curso);
     }
 
+    @Operation(summary = "Atualizar curso")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Atualizar pelo id do curso")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Curso atualizado com sucesso", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CursoCriar.class))),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @PutMapping("/{id}")
     public ResponseEntity<CursoBuscar> editarCursos(@PathVariable UUID id, @RequestBody @Valid CursoCriar dto) {
         CursoBuscar cursoEditado = cursoService.editarCurso(id, dto);
         return ResponseEntity.ok(cursoEditado);
     }
 
+    @Operation(summary = "Remover curso")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Remover pelo id do curso")})
+    @ApiResponses(value = {
+            @ApiResponse(description = "Curso removido com sucesso", responseCode = "204"),
+            @ApiResponse(description = "Dados informados inválidos", responseCode = "400", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Dados não encontrados", responseCode = "404", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(description = "Erro interno no servidor", responseCode = "500", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarCurso(@PathVariable UUID id) {
         cursoService.deletarCurso(id);
