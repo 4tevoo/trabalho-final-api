e1495873340f9389a0f83fc41c4a98ba70441857
yasmimverissimo <yasmim.verissimo@aluno.senai.br>
2026-05-27 15:51:15 -0300

    Clean up CursoRepository by removing unused methods
    Removed unused query methods from CursoRepository.

--- src/main/java/org/serratec/coldmart/repository/CursoRepository.java
+++ src/main/java/org/serratec/coldmart/repository/CursoRepository.java
@@ -8,9 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, UUID> {
-    List<Curso> findByTituloContainingIgnoreCase(String titulo);
-    List<Curso> findByCategoriaId(UUID categoriaId);
-
-    boolean existsByTituloContainingIgnoreCase(String termo);
     boolean existsByTituloAndCategoriaId(String titulo, UUID categoriaId);
 }
